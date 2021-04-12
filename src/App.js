d3c6b4f9bb4240709f8e384fc10d13ad85848aa8
Qadree Trimble <qadree_t@yahoo.com>
2021-04-12 10:44:25 -0700

    building App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
+import Search from './Search.js';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state={
+            alreadySearched: false,
+            searchedCity: '',
+        };
+    }
+    handleShowSearch = () => {
+        this.setState({alreadySearched: false});
+    }
+    handleSearch = (searchedCity) => {
+        console.log('search', searchedCity);
+        this.setState({
+            alreadySearched: true,
+            searchedCity: searchedCity
+        });
+        let locationAnswerData = await axios.get('');
+    }
     render () {
         return(
-            <h1>Hello WORLD</h1>
+            <>
+            <h1>City Explorer</h1>
+            {this.state.alreadySearched ?
+            <button onClick={this.handleShowSearch}>Search Again</button> :
+            <Search handleSearch={this.handleSearch} />}
+            </>
         );
     }
 }
